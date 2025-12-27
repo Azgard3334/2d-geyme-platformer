5d268cf60c9334a01d0e2c6e170a8934e59d2809
Askar <rizatdinov06@mail.ru>
2025-12-27 19:32:53 +0300

    Kinematic management

--- kinematic_player.gd
+++ kinematic_player.gd
@@ -1,16 +1,54 @@
 extends RigidBody2D
 
-var thrust = Vector2(0, -250)
-var torque = 20000
+@export var move_force = 5000.0
+@export var jump_force = 500.0
+@export var max_speed = 800.0
 
-func _integrate_forces(state):
-	if Input.is_action_pressed("jump"):
-		state.apply_force(thrust.rotated(rotation))
-	else:
-		state.apply_force(Vector2())
-	var rotation_direction = 0
+var screen_size: Vector2
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+	linear_damp = 1.0
+	set_linear_damp_mode(DampMode.DAMP_MODE_REPLACE)
+
+	lock_rotation = true
+
+	$CollisionShape2D.disabled = false
+
+func _physics_process(delta):
+	var force = Vector2.ZERO
+
 	if Input.is_action_pressed("rightMove"):
-		rotation_direction += 1
-	if Input.is_action_pressed("leftMove"):
-		rotation_direction -= 1
-	state.apply_torque(rotation_direction * torque)
+		force.x += move_force
+	elif Input.is_action_pressed("leftMove"):
+		force.x -= move_force
+
+	if Input.is_action_just_pressed("jump"):
+		apply_central_impulse(Vector2(0, -jump_force))
+
+	if force != Vector2.ZERO:
+		apply_central_force(force)
+
+	if linear_velocity.length() > max_speed:
+		linear_velocity = linear_velocity.normalized() * max_speed
+
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+	update_animation()
+
+func update_animation():
+	if abs(linear_velocity.x) > 10:
+		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.animation = "rightMove"
+		$AnimatedSprite2D.flip_h = linear_velocity.x < 0
+	else:
+		$AnimatedSprite2D.stop()
+
+func is_on_floor() -> bool:
+	return linear_velocity.y == 0 and position.y >= screen_size.y - 50
+
+func start(pos):
+	position = pos
+	linear_velocity = Vector2.ZERO
+	$CollisionShape2D.disabled = false
--- main.gd
+++ main.gd
@@ -1,7 +1,7 @@
 extends Node
 
-func new_game():
-	$player.start($startPosition.position)
+func newGame():
+	$kinematicPlayer.start($startPosition.position)
 
 func _ready():
-	new_game()
+	newGame()
